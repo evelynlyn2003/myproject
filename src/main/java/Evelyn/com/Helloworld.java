02e1b2b9ed2424ae383ace71c835bc8e68cfe30f
Evelyn <reser920215@gmail.com>
2022-03-04 15:32:11 +0800

    initial commit

---
+++ src/main/java/Evelyn/com/Helloworld.java
@@ -0,0 +1,13 @@
+package Evelyn.com;
+
+public class Helloworld {
+    public static void main(String[] args) {
+        String[] names = {"jack", "Hank", "Elva", "Benny"};
+        int[] english = {70,80,90,60,48};
+        int[] maths={88,97,54,32,75};
+        for (int i = 0;i<5;i++){
+            System.out.println(names[i] + "\t" + english[i] + "\t" + maths[i] );
+
+        }
+    }
+}
