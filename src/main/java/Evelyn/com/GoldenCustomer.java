bdbc1d92d3c4df26c45d80d6971fa5894c0de603
Evelyn <reset920215@gmail.com>
2022-03-18 16:22:07 +0800

    initial commit

---
+++ src/main/java/Evelyn/com/GoldenCustomer.java
@@ -0,0 +1,8 @@
+package Evelyn.com;
+
+public class GoldenCustomer  extends SilverCustomer{
+    public GoldenCustomer (String id,int amount){
+        super(id, amount);
+        off = 0.2f;
+    }
+}
--- src/main/java/Evelyn/com/SilverCustomer.java
+++ src/main/java/Evelyn/com/SilverCustomer.java
@@ -4,5 +4,11 @@ public class SilverCustomer extends Customer{
     public SilverCustomer(String id, int amount) {
         super(id, amount);
     }
+        @Override
+        public void print() {
+            System.out.println(id + "\t" + amount + "\t" + (amount-backMoney())
+                    +"("+backMoney()+")");
+
+    }
 
 }
